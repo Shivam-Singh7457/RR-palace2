fa43d8b7f9de96e5a18beeadc727328181f5c6b2
Shivam Singh <shivamsinghsn95@gmail.com>
2025-07-03 13:10:07 +0530

    Update owner.js new coowner

--- server/configs/owner.js
+++ server/configs/owner.js
@@ -1,3 +1,3 @@
 // config/owners.js
 export const OWNER_ID = "user_2zC53qU7LRjRNJDmFHtamnHFWJV"; // main owner Clerk ID
-export const COOWNERS = ["user_2z0JoD8p0A59Tpcid9Be4B8tvGQ"]; // array of co-owner Clerk IDs
+export const COOWNERS = ["user_2z0JoD8p0A59Tpcid9Be4B8tvGQ", "user_2z3QQ6yL8D3jVZvp2KxEwF7x9YO"]; // array of co-owner Clerk IDs
