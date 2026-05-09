2ab9cd4902fad99ed8a7f14e96f3fa253d6151ec
Shivam Singh <shivamsinghsn95@gmail.com>
2026-05-09 13:15:18 +0530

    final final

--- server/configs/nodemailer.js
+++ server/configs/nodemailer.js
@@ -2,8 +2,8 @@ import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 465,
-    secure: true, // Use SSL/TLS
+    port: process.env.SMTP_PORT || 465,
+    secure: (process.env.SMTP_PORT == 465), // true for 465, false for other ports
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
